ca95b0d06ee020fd171d0ecb39bce68973af3adb
DanWuPet <danx1698@edu.easj.dk>
2021-09-08 11:17:14 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void zoop() {
+        baffle();
+        System.out.print("You wugga ");
+        baffle();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("No, I ");
+        zoop();
+        System.out.print("I ");
+        baffle();
+    }
+
+    public static void baffle() {
+        System.out.print("wug");
+        ping();
+    }
+
+    public static void ping() {
+        System.out.println(".");
+    }
+}
+
+//1. baffle. Output (No, I wug)
+//2. No, I wug. You wugga wug.I wug.
